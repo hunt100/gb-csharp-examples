60239a5ee54c643b62bc0916f6ee8d580a2b01ec
Anvar HunT <anvar.hunt@gmail.com>
2023-01-15 13:01:43 +0600

    Add Homework001
    Сhange encoding of 1 old file

---
+++ Homework1-Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Write your firstNumber: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Write your secondNumber: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int max = firstNumber > secondNumber ? firstNumber : secondNumber;
+Console.WriteLine($"max = {max}");
---
+++ Homework1-Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+    
+Console.WriteLine("Write your firstNumber: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Write your secondNumber: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Write your thirdNumber: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+int max;
+if (firstNumber > secondNumber && firstNumber > thirdNumber)
+{
+    max = firstNumber;
+} else if (secondNumber > firstNumber && secondNumber > thirdNumber)
+{
+    max = secondNumber;
+}
+else
+{
+    max = thirdNumber;
+}
+
+Console.WriteLine($"max = {max}");
