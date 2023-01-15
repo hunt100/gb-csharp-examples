4a68991ad683955fd286b7f4b8962b23bbe5e782
Anvar HunT <anvar.hunt@gmail.com>
2023-01-15 18:14:22 +0600

    Solve task with rand number

---
+++ Seminar2-1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8 
+// 12-> 2 
+// 85 -> 8
+
+int randNum = new Random().Next(10, 100);
+
+int firstReminder = randNum / 10;
+int secondReminder = randNum % 10;
+
+int max = firstReminder > secondReminder ? firstReminder : secondReminder;
+Console.WriteLine($"Generated number: {randNum}");
+Console.WriteLine("Max: " + max);
