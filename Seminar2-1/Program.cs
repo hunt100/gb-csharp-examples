d286447736be5282af224b08581c7c48248d7b36
Anvar HunT <anvar.hunt@gmail.com>
2023-01-15 18:22:04 +0600

    Solve task with division with reminder

--- Seminar2-1/Program.cs
+++ Seminar2-1/Program.cs
@@ -1,13 +1,14 @@
-﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
-// 78 -> 8 
-// 12-> 2 
-// 85 -> 8
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому.
+// Если число 2 не кратно числу 1, то программа выводит остаток от деление.
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
 
-int randNum = new Random().Next(10, 100);
+Console.WriteLine("Input first num: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
 
-int firstReminder = randNum / 10;
-int secondReminder = randNum % 10;
+Console.WriteLine("Input second num: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
 
-int max = firstReminder > secondReminder ? firstReminder : secondReminder;
-Console.WriteLine($"Generated number: {randNum}");
-Console.WriteLine("Max: " + max);
+Console.WriteLine(firstNum % secondNum == 0
+    ? "Division without reminder"
+    : $"Division with reminder! Reminder: {firstNum % secondNum}");
