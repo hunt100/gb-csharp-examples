ec412cb04d5ad0b9419600cba4d2c9a94e388251
Anvar HunT <anvar.hunt@gmail.com>
2023-01-29 03:22:31 +0600

    Add Homework 5

---
+++ Homework5-Task1/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//
+// [345, 897, 568, 234] -> 2
+
+Random random = new Random();
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] CreateArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(100, 1000);
+    }
+    return arr;
+}
+
+int CountEvenNumbers(int[] arr)
+{
+    int counter = 0;
+    foreach (int num in arr)
+    {
+        if (num % 2 == 0)
+        {
+            counter++;
+        }
+    }
+
+    return counter;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i]);
+        if (i == arr.Length - 1)
+        {
+            break;
+        }
+        Console.Write(", ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int size = Prompt("Write size of array:");
+int[] arr = CreateArray(size);
+int evenNumberCount = CountEvenNumbers(arr);
+PrintArray(arr);
+Console.WriteLine($"Even number count: {evenNumberCount}");
