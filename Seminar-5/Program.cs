a3f18d27eae73f47ab587a578aaf033c33c6cf29
Anvar HunT <anvar.hunt@gmail.com>
2023-01-29 18:13:39 +0600

    Task 2

--- Seminar-5/Program.cs
+++ Seminar-5/Program.cs
@@ -1,49 +1,33 @@
-﻿// Напишите программу, которая принимает на вход три числа и проверяет,
-// может ли существовать треугольник с сторонами такой длины.
+﻿// Задача 2: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
 
-double Prompt(string message)
+int Prompt(string message)
 {
     Console.WriteLine(message);
-    return Convert.ToDouble(Console.ReadLine());
+    return Convert.ToInt32(Console.ReadLine());
 }
 
-void PrintArray(double[] arr)
+void PrintAsBinaryNum(int num)
 {
-    Console.Write("[");
-    for (int i = 0; i < arr.Length; i++)
+    string binStr = "";
+    if (num == 0)
     {
-        Console.Write(arr[i]);
-        if (i == arr.Length - 1)
-        {
-            break;
-        }
-        Console.Write(", ");
+        Console.WriteLine("0");
+        return;
     }
-    Console.Write("]");
-    Console.WriteLine();
-}
-
-double[] edgeLength = {
-    Prompt("Write 1 length:"),
-    Prompt("Write 2 length:"),
-    Prompt("Write 3 length:")
-};
-
-bool IsTriangleExist(double[] edgeLength)
-{
-    for (int i = 0; i < edgeLength.Length; i++)
+    
+    while (num > 0)
     {
-        if (edgeLength[0] > edgeLength[1] + edgeLength[2])
-        {
-            return false;
-        }
-
-        edgeLength = edgeLength.Skip(1).Concat(edgeLength.Take(1)).ToArray();
+        int reminder = num % 2;
+        binStr += reminder;
+        num /= 2;
     }
 
-    return true;
+    Console.WriteLine($"Own bin: {new string(binStr.Reverse().ToArray())}");
 }
 
-PrintArray(edgeLength);
-bool isValid = IsTriangleExist(edgeLength);
-Console.WriteLine($"is valid triangle: {isValid}");
+int num = Prompt("Write a number: ");
+Console.WriteLine($"binary: {Convert.ToString(num, 2)}");
+PrintAsBinaryNum(num);
